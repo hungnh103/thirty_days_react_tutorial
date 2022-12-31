ff9e634ebd87e881168dc2d928c4c4b976e2cb56
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2022-12-31 08:49:28 +0700

    day09, exercise 3.1

--- src/index.js
+++ src/index.js
@@ -1,41 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-class App extends React.Component {
-  state = {
-    backgroundImg: null
-  }
+const Button = ({text, clickme}) => (
+  <div>
+    <button onClick={clickme}>
+      {text}
+    </button>
+  </div>
+)
 
+const Loaded = () => (
+  <span>data loaded successfully</span>
+)
+
+const Loading = () => (
+  <img src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921' alt='loading-icon' />
+)
+
+const NotLoad = () => (
+  <span>please click button to load data</span>
+)
+
+class Main extends React.Component {
   render() {
-    const season = this.state.backgroundImg ? this.state.backgroundImg : prompt('which season?')
-    switch(season) {
-    case 'spring':
-      this.setState({backgroundImg: 'https://www.vietnam-visa.com/wp-content/uploads/2017/12/spring-in-vietnam-hanoi-compressor.jpg'})
-      break
-    case 'summer':
-      this.setState({backgroundImg: 'https://hips.hearstapps.com/hmg-prod/images/when-does-summer-end-1651614198.jpg?crop=0.656xw:1.00xh;0.173xw,0&resize=480:*'})
-      break
-    case 'autumn':
-      this.setState({backgroundImg: 'https://cdn.britannica.com/88/137188-050-8C779D64/Boston-Public-Garden.jpg'})
-      break
-    case 'winter':
-      this.setState({backgroundImg: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/trees-on-snow-covered-landscape-in-heavy-snow-day-royalty-free-image-1634585533.jpg?crop=0.668xw:1.00xh;0.167xw,0&resize=640:*'})
-      break
-    default:
-      break
+    const current_state = this.props.loadDataStatus
+    let content
+
+    if(current_state === true) {
+      content = <Loaded />
+    } else if(current_state === 'loading') {
+      content = <Loading />
+      setTimeout(this.props.loaded, 2 * 1000)
+    } else if(current_state === false) {
+      content = <NotLoad />
     }
 
     return (
+      <div style={{margin: 10}}>
+        {content}
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    data_loaded: false
+  }
+
+  fetchData = () => {
+    this.setState({data_loaded: 'loading'})
+  }
+
+  fetchedData = () => {
+    this.setState({data_loaded: true})
+  }
+
+  render() {
+    return (
       <div className='app'>
-        <div
-          className='illustration'
-          style={{
-            background: `url(${this.state.backgroundImg})`,
-            width: '200px',
-            height: '200px'
-          }}
-        >
-        </div>
+        <Button clickme={this.fetchData} text='get data from server' />
+        <Main loadDataStatus={this.state.data_loaded} loaded={this.fetchedData} />
       </div>
     )
   }
