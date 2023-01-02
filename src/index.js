7245b8a9bb98e289f93000643e6bcb09be1348cd
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2023-01-02 11:40:33 +0700

    day 11, theory

--- src/index.js
+++ src/index.js
@@ -1,8 +1,90 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends Component {
+  state = {
+    message: '',
+    value: '',
+  }
+
+  handleClick = () => {
+    this.setState({
+      message: 'Welcome to the world of events'
+    })
+  }
+
+  handleMouseMove = () => {
+    this.setState({
+      message: 'mouse is moving'
+    })
+  }
+
+  handleCopy = () => {
+    this.setState({
+      message: 'using 30 days of react for commercial purpose is not allowed'
+    })
+  }
+
+  handleKeyPress = (e) => {
+    this.setState({
+      message:
+        `${e.target.value} has been pressed and the keycode is ${e.charCode}`
+    })
+  }
+
+  handleBlur = () => {
+    this.setState({
+      message: 'Input field has been blurred'
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  handleChange = (e) => {
+    this.setState({message: e.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the World of Events</h1>
+
+        <button onClick={this.handleClick}>Click Me</button>
+        <br />
+        <br />
+        <button onMouseMove={this.handleMouseMove}>Move mouse on me</button>
+        <p onCopy={this.handleCopy}>
+          Check copyright permission by copying this text
+        </p>
+
+        <label htmlFor="">Test for onKeyPress Event:</label>
+        <input type="text" onKeyPress={this.handleKeyPress} />
+        <br />
+
+        <label htmlFor="">Test for onBlur Event:</label>
+        <input type="text" onBlur={this.handleBlur} />
+
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              onChange={this.handleChange}
+              id='firstName'
+            />
+          </div>
+
+          <div>
+            <input type="submit" value='Submit' />
+          </div>
+        </form>
+
+        <p>{this.state.message}</p>
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +92,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
