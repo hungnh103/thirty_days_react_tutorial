cbf50c0be5d05fcf4f1cb6013dfcb39614decce7
Nguyen Huy Hung <hungnh103@gmail.com>
2023-01-03 23:38:24 +0700

    axios demo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
+
+class App extends Component {
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    const API_URL = 'https://restcountries.com/v2/regionalbloc/eu?fields=name,flag,capital,population,languages'
+
+    axios.get(API_URL)
+         .then((response) => {
+           this.setState({ data: response.data })
+         })
+         .catch((error) => {
+           console.log(error)
+         })
+  }
+
+  renderCountries = () => {
+    return this.state.data.map((country) => {
+      const languageOrLanguages =
+        country.languages.length > 1 ? 'Languages' : 'Language'
+      const formatLanguages = country.languages.map(({ name }) => name).join(', ')
+
+      return (
+        <div key={country.name}>
+          <div>
+            <img src={country.flag} alt={country.name} />
+          </div>
+
+          <div>
+            <h1>{country.name}</h1>
+            <p>Capital: {country.capital}</p>
+            <p>
+              {languageOrLanguages}: {formatLanguages}
+            </p>
+            <p>Population: {country.population}</p>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Fetching Data Using Axios</h1>
+        <div>
+          <p>There are {this.state.data.length} countries in the API</p>
+          <div className='countries-wrapper'>{this.renderCountries()}</div>
+        </div>
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +63,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
