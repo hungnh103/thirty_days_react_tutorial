c21324eaad0d99c19e6a26b5f91be5079c2bd2df
Nguyen Huy Hung <hungnh103@gmail.com>
2023-01-03 23:52:16 +0700

    react-icons demo

--- src/index.js
+++ src/index.js
@@ -1,57 +1,31 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
-import axios from 'axios'
+import {
+  TiSocialLinkedinCircular,
+  TiSocialGithubCircular,
+  TiSocialTwitterCircular
+} from 'react-icons/ti'
+
+const Footer = () => (
+  <footer>
+    <h3>30 Days Of React</h3>
+    <div>
+      <TiSocialLinkedinCircular />
+      <TiSocialGithubCircular />
+      <TiSocialTwitterCircular />
+    </div>
+    <div>
+      <small>Copyright &copy; {new Date().getFullYear()}</small>
+    </div>
+  </footer>
+)
 
 class App extends Component {
-  state = {
-    data: []
-  }
-
-  componentDidMount() {
-    const API_URL = 'https://restcountries.com/v2/regionalbloc/eu?fields=name,flag,capital,population,languages'
-
-    axios.get(API_URL)
-         .then((response) => {
-           this.setState({ data: response.data })
-         })
-         .catch((error) => {
-           console.log(error)
-         })
-  }
-
-  renderCountries = () => {
-    return this.state.data.map((country) => {
-      const languageOrLanguages =
-        country.languages.length > 1 ? 'Languages' : 'Language'
-      const formatLanguages = country.languages.map(({ name }) => name).join(', ')
-
-      return (
-        <div key={country.name}>
-          <div>
-            <img src={country.flag} alt={country.name} />
-          </div>
-
-          <div>
-            <h1>{country.name}</h1>
-            <p>Capital: {country.capital}</p>
-            <p>
-              {languageOrLanguages}: {formatLanguages}
-            </p>
-            <p>Population: {country.population}</p>
-          </div>
-        </div>
-      )
-    })
-  }
-
   render() {
     return (
       <div className='App'>
-        <h1>Fetching Data Using Axios</h1>
-        <div>
-          <p>There are {this.state.data.length} countries in the API</p>
-          <div className='countries-wrapper'>{this.renderCountries()}</div>
-        </div>
+        <h1>Welcome to the world of Icons</h1>
+        <Footer />
       </div>
     )
   }
