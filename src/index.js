525091480d4ccba9a6db4bec8e68bdde627bbf70
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2023-01-03 07:44:16 +0700

    day14, theory: updating phase

--- src/index.js
+++ src/index.js
@@ -5,63 +5,40 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      firstName: 'John',
-      data: []
+      day: 1,
+      congratulate: null
     }
   }
 
-  renderCountries = () => {
-    return this.state.data.map((country) => {
-      return (
-        <div
-          key={country.name}
-          style={{float: 'left', margin: 20, height: 400}}
-        >
-          <div>
-            <img
-              src={country.flag}
-              alt={country.name}
-              style={{width: 200}}
-            />
-          </div>
-          <div>
-            <h1>{country.name}</h1>
-            <p>Capital: {country.capital}</p>
-            <p>Population: {country.population}</p>
-          </div>
-        </div>
-      )
-    })
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.day > 31) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  doChallenge = () => {
+    this.setState({ day: this.state.day + 1 })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.day === 30) {
+      this.setState({ congratulate: 'chuc mung hoan thanh!' })
+    }
+
+    console.log(prevState, prevProps)
   }
 
   render() {
     return (
-      <div className='App'>
+      <div>
         <h1>React Component Life Cycle</h1>
-        <h1>Calling API</h1>
-        <div>
-          <p>There are {this.state.data.length} countries in the API</p>
-          <div className='countries-wrapper'>
-            {this.renderCountries()}
-          </div>
-        </div>
+        <button onClick={this.doChallenge}>Do Challenge</button>
+        <p>Challenge: Day {this.state.day}</p>
+        {this.state.congratulate && <h2>{this.state.congratulate}</h2>}
       </div>
     )
-  }
-
-  componentDidMount() {
-    const API_URL = 'https://restcountries.com/v2/regionalbloc/eu?fields=name,flag,capital,population'
-
-    fetch(API_URL)
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-        this.setState({ data })
-      })
-      .catch((error) => {
-        console.log(error)
-      })
   }
 }
 
