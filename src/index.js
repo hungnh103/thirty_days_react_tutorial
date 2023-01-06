67014413533838b18e7eb421b599ff4d2bd93c11
Nguyen Huy Hung <hungnh103@gmail.com>
2023-01-06 07:50:44 +0700

    day 18, theory: fetch

--- src/index.js
+++ src/index.js
@@ -1,8 +1,102 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Country = ({
+  country: { name, capital, flag, languages, population, currency }
+}) => {
+  const formatedCapital =
+    capital.length > 0 ? (
+      <>
+        <span>Capital: </span>
+        {capital}
+      </>
+    ) : (
+      ''
+    )
+
+  const formatLanguage = languages.length > 1 ? `Languages` : `Language`
+
+  return (
+    <div className='country'>
+      <div className='country_flag'>
+        <img src={flag} alt={name} />
+      </div>
+
+      <h3 className='country_name'>{name.toUpperCase()}</h3>
+
+      <div className='country_text'>
+        <p>{formatedCapital}</p>
+
+        <p>
+          <span>{formatLanguage}: </span>
+          {languages.map((language) => language.name).join(', ')}
+        </p>
+
+        <p>
+          <span>Population: </span>
+          {population.toLocaleString()}
+        </p>
+
+        <p>
+          <span>Currency: </span>
+          {currency}
+        </p>
+      </div>
+    </div>
+  )
+}
+class App extends Component {
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    this.fetchCountryData()
+
+    // const url = 'https://restcountries.com/v2/regionalbloc/al?fields=name,capital,flag,languages,population,currencies'
+
+    // fetch(url)
+    //   .then((response) => {
+    //     return response.json()
+    //   })
+    //   .then((data) =>{
+    //     console.log(data)
+    //     this.setState({ data })
+    //   })
+    //   .catch((error) => {
+    //     console.log(error)
+    //   })
+  }
+
+  fetchCountryData = async () => {
+    const url = 'https://restcountries.com/v2/regionalbloc/al?fields=name,capital,flag,languages,population,currencies'
+
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      this.setState({ data })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>React Component Life Cycle</h1>
+        <h1>Calling API</h1>
+        <div>
+          <p>There are {this.state.data.length} countries in the API</p>
+          <div className='countries-wrapper'>
+            {this.state.data.map((country) => (
+              <Country key={Math.random()} country={country} />
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +104,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
