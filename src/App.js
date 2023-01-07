fcdf17608fd8cdee39323bd911d8c342b846cc00
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2023-01-07 10:18:14 +0700

    day 17, theory

--- src/App.js
+++ src/App.js
@@ -1,25 +1,298 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  NavLink,
+  Redirect,
+  Prompt,
+  withRouter,
+} from 'react-router-dom'
 
-function App() {
+const Home = (props) => <h1>Welcome Home</h1>
+
+const About = (props) => <h1>About Us</h1>
+
+const Contact = (props) => <h1>Contact Us</h1>
+
+const challenges = [
+  {
+    name: '30 Days Of Python',
+    description:
+      '30 Days of Python challenge is a step by step guide to learn Python in 30 days.',
+    status: 'completed',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '20 Nov 2019 - 20 Dec 2019',
+    slug: 'pyhton',
+    url:
+      'https://github.com/Asabeneh/30-Days-Of-Python.com',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+  {
+    name: '30 Days Of JavaScript',
+    description:
+      '30 Days of JavaScript challenge is a step by step guide to learn JavaScript in 30 days.',
+    status: 'completed',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '1 Jan 2020 - 30 Jan 2020',
+    slug: 'javascript',
+    url: 'https://github.com/Asabeneh/30-Days-Of-JavaScript',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+  {
+    name: '30 Days Of React',
+    description:
+      '30 Days of React challenge is a step by step guide to learn React in 30 days.',
+    status: 'ongoing',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '1 Oct 2020- 30 Oct 2020',
+    slug: 'react',
+    url: 'https://github.com/Asabeneh/30-Days-Of-React',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+  {
+    name: '30 HTML and CSS',
+    description:
+      '30 Days of HTML and CSS challenge is a step by step guide to learn HTML and CSS in 30 days.',
+
+    status: 'coming',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '',
+    slug: 'html-and-css',
+    url: '',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+  {
+    name: '30 ReactNative',
+    description:
+      '30 Days of ReactNative challenge is a step by step guide to learn ReactNative in 30 days.',
+    status: 'coming',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '',
+    slug: 'reactnative',
+    url: '',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+  {
+    name: '30 Data Analysis',
+    description:
+      '30 Days of Data Analysis challenge  is a step by step guide to learn about data, data visualization and data analysis in 30 days.',
+    status: 'coming',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '',
+    slug: 'data-analysis',
+    url: '',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+  {
+    name: '30 Machine Learning',
+    description:
+      '30 Days of Machine learning challenge  is a step by step guide to learn data cleaning, machine learning models and predictions in 30 days.',
+    status: 'coming',
+    days: 30,
+    level: 'Beginners to Advanced',
+    duration: '',
+    slug: 'machine-learning',
+    url: '',
+    author: {
+      firstName: 'Asabeneh',
+      lastName: 'Yetayeh',
+    },
+  },
+]
+
+const Challenge = ({
+  challenge: {
+    name,
+    description,
+    status,
+    days,
+    level,
+    duration,
+    author: { firstName, lastName }
+  }
+}) => (
+  <div>
+    <h1>{name}</h1>
+    <p>{level}</p>
+    <p>
+      Author: {firstName} {lastName}
+    </p>
+    {duration && (
+      <>
+        <small>{duration}</small>
+        <br />
+      </>
+    )}
+    <small>Number of days: {days}</small>
+    <p>{description}</p>
+  </div>
+)
+
+const Challenges = (props) => {
+  const path = props.location.pathname
+  const slug = path.split('/').slice(path.split('/').length -1)[0]
+  const challenge = challenges.find((challenge) => challenge.slug === slug)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>30 Days Of React Challenge</h1>
+
+      <ul>
+        {challenges.map(({ name, slug }) => (
+          <li key={name}>
+            <NavLink to={`/challenges/${slug}`}>{name}</NavLink>
+          </li>
+        ))}
+      </ul>
+
+      <Switch>
+        <Route
+          exact
+          path={'/challenges'}
+          component={() => <h1>Choose any of the challenges</h1>}
+        />
+        <Route
+          path={path}
+          component={(props) => <Challenge challenge={challenge} />}
+        />
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+const NotFound = (props) => <h1>The page you are looking for not found</h1>
+
+const NavBar = ({ username }) => (
+  <ul>
+    <li>
+      <NavLink to='/'>Home</NavLink>
+    </li>
+    <li>
+      <NavLink to='/about'>About</NavLink>
+    </li>
+    <li>
+      <NavLink to='/contact'>Contact</NavLink>
+    </li>
+    <li>
+      <NavLink to={`/user/${username}`}>User</NavLink>
+    </li>
+    <li>
+      <NavLink to='/challenges'>Challenges</NavLink>
+    </li>
+  </ul>
+)
+
+const User = ({ match, isLoggedIn, handleLogin }) => {
+  const username = match.params.username
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <>
+          <h1>Welcome {username} to the challenge</h1>
+          <small>Now, you can navigate through all the challenges</small>
+          <br />
+        </>
+      ) : (
+        <p>Please login to access the challenges</p>
+      )}
+
+      <button onClick={handleLogin}>{isLoggedIn ? 'Logout' : 'Login'}</button>
+    </div>
+  )
+}
+
+const Welcome = ({ isLoggedIn, handleLogin }) => {
+  return (
+    <div>
+      {isLoggedIn ? 'Welcome to the challenge' : <p>Please login</p>}
+      <button onClick={handleLogin}>{isLoggedIn ? 'Logout' : 'Login'}</button>
+    </div>
+  )
+}
+class App extends Component {
+  state = {
+    isLoggedIn: false,
+    firstName: 'Robert'
+  }
+
+  handleLogin = () => {
+    this.setState({
+      isLoggedIn: !this.state.isLoggedIn
+    })
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className='app'>
+          <NavBar username={this.state.firstName} />
+
+          <Switch>
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route
+              path='/user/:username'
+              component={(props) => (
+                <User
+                  {...props}
+                  isLoggedIn={this.state.isLoggedIn}
+                  handleLogin={this.handleLogin}
+                />
+              )}
+            />
+            <Route
+              path='/login'
+              component={(props) => (
+                <Welcome
+                  {...props}
+                  isLoggedIn={this.state.isLoggedIn}
+                  handleLogin={this.state.handleLogin}
+                />
+              )}
+            />
+            <Route
+              path='/challenges'
+              component={(props) => {
+                return this.state.isLoggedIn ? (
+                  <Challenges {...props} />
+                ) : (
+                  <Redirect to='/user/Robert' />
+                )
+              }}
+            />
+            <Route exact path='/' component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App
