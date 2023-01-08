cb1d4d3fae7ef5c7721daa0b13d753e75eedd48b
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2023-01-08 13:03:05 +0700

    day 23, theory

--- src/index.js
+++ src/index.js
@@ -1,8 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
+
+const Country = ({ country: { name, flag, population } }) => {
+  return (
+    <div className='country'>
+      <div className='country_flag'>
+        <img src={flag} alt={name} />
+      </div>
+
+      <h3 className='country_name'>{name.toUpperCase()}</h3>
+
+      <div className='country_population'>
+        <p>
+          <span>Population: </span>
+          {population.toLocaleString()}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+const App = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const url = 'https://restcountries.com/v2/regionalbloc/pa?fields=name,flag,population'
+
+    try {
+      const response = await axios.get(url)
+      const data = await response.data
+      setData(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className='App'>
+      <h1>Fetching Data Using Hook</h1>
+      <h1>Calling API</h1>
+      <div>
+        <p>There are {data.length} countries in the API</p>
+        <div className='countries-wrapper'>
+          {data.map((country) => (
+            <Country key={Math.random()} country={country} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +62,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
